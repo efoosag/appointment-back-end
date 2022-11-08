024c16b954f190b9c27c78cfab996be3884f327e
efoosag <efoosagsite@gmail.com>
2022-11-08 19:56:02 +0100

    remove devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  include Devise::JWT::RevocationStrategies::JTIMatcher
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
-  
-  def jwt_payload
-    super
-  end       
+      
 end
