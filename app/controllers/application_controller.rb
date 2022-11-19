7dfd8c07c3d9c3c2b21be78c841aa33a4e2499e2
Gideon <gideons564@gmail.com>
2022-11-19 17:04:43 +0100

    Add user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-   include JwtToken
+  include JwtToken
 
   before_action :authenticate_user
 
@@ -7,14 +7,14 @@ class ApplicationController < ActionController::API
 
   def authenticate_user
     header = request.headers['Authorization']
-    header = header.split(' ').last if header
+    header = header.split.last if header
     begin
-    @decoded = JwtToken.decode(header)
-    @current_user = User.find(@decoded[:user_id])
-  rescue ActiveRecord::RecordNotFound => e 
-    render json: { errors: e.message }, status: :unauthorized
-  rescue JWT::DecodeError => e 
-    render json: { errors: e.message }, status: :unauthorized
+      @decoded = JwtToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
- end
 end
--- app/controllers/concerns/authentication_controller.rb
+++ app/controllers/concerns/authentication_controller.rb
@@ -6,10 +6,10 @@ class AuthenticationController < ApplicationController
     if @user&.authenticate(params[:Password])
       token = JwtToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), 
-                              username: @user.user_name }, status: :ok
-      else
-        render json: {error: "unauthorized"}, status: :unauthorized
-      end
+      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     username: @user.user_name }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
   end
 end
